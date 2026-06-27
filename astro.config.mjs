99a52d89d129522b2780da0bc17aac63f2968489
Cesar <cesaramuroc@gmail.com>
2026-06-26 21:23:50 -0400

    update fotos coachs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  site: "https://www.theballplex.com",
   vite: {
     plugins: [tailwindcss()],
   },
